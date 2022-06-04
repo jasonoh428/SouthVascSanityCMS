900dbece5c7a572555103ed51339d745fa08e486
jasonoh428 <ohjason794@gmail.com>
2022-06-04 12:08:01 -0500

    changed sublink type

--- schemas/objects/subLink.js
+++ schemas/objects/subLink.js
@@ -1,44 +1,42 @@
 export default {
-    name: "subLink",
-    title: "Child Navigation Link",
-    type: "object",
-    fields: [
-      {
-        name: "display",
-        title: "Display Name",
-        type: "string",
-      },
-  
-      {
-        name: "internal",
-        title: "Link is Internal?",
-        type: "boolean",
-      },
+  name: "subLink",
+  title: "Child Navigation Link",
+  type: "object",
+  fields: [
+    {
+      name: "display",
+      title: "Display Name",
+      type: "string",
+    },
 
-      {
-        name:'file',
-        type:'file',
-        title:'File'
-      },
+    {
+      name: "internal",
+      title: "Link is Internal?",
+      type: "boolean",
+    },
 
-      {
-        name:'externalLink',
-        type:'url',
-        title:'External Link'
-      },
-  
-      {
-        name: "internalPage",
-        type: "reference",
-        title: "Page Link if Internal",
-        to: [
-          {
-            type: "page",
-          },
-        ],
-      },
-  
-      
-    ],
-  };
+    {
+      name: "file",
+      type: "file",
+      title: "File",
+    },
+
+    {
+      name: "externalLink",
+      type: "string",
+      title: "External Link",
+    },
+
+    {
+      name: "internalPage",
+      type: "reference",
+      title: "Page Link if Internal",
+      to: [
+        {
+          type: "page",
+        },
+      ],
+    },
+  ],
+};
   
