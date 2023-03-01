71c2f9d43afbe771acc9f8ada06cce0807c15cca
jasonoh428 <ohjason794@gmail.com>
2023-03-02 02:12:55 +0500

    added fields for SEO team

---
+++ schemas/documents/seo.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'seo',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+      },
+      {
+        name: 'description',
+        type: 'text',
+        title: 'Description',
+      },
+      {
+        name: 'keywords',
+        type: 'string',
+        title: 'Keywords',
+      },
+      {
+        name: 'headCode',
+        type: 'text',
+        title: 'Head Code',
+      },
+      {
+        name: 'bodyCode',
+        type: 'text',
+        title: 'Body Code',
+      },
+    ],
+  };
+  
